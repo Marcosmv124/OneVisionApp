82f166f0eca439accb4c3c7dc99ef6a42e971325
Marcomv124 <l21210398@tectijuana.edu.mx>
2025-05-01 20:48:19 -0700

    MODIFICACIONES GENERALES

--- Controllers/PacientesController.cs
+++ Controllers/PacientesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -17,7 +18,7 @@ namespace One_Vision.Controllers
         {
             _context = context;
         }
-
+        [Authorize]
         // GET: Pacientes
         public async Task<IActionResult> Index()
         {
